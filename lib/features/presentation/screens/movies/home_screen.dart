340ed01f5d17e0a38938d52a6a8027585a7a5c9a
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 <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>
2025-08-09 10:34:45 -0500

    environment variables configure

--- lib/features/presentation/screens/movies/home_screen.dart
+++ lib/features/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
@@ -7,6 +9,10 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: const Placeholder());
+    return Scaffold(
+      body: Center(
+        child: Text(Environment.theMovieDvKEy),
+      )
+      );
   }
 }
